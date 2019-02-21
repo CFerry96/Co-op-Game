b777dd3585047df55bcb0afbc5335ea7ca290f39
CFerry <c16349346@mydit.ie>
2019-02-21 15:40:51 +0000

    First Push

---
+++ Co-op/Assets/MassChanger.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MassChanger : MonoBehaviour
+{
+
+    public Rigidbody rb1;
+    public Transform p1;
+
+    public Rigidbody rb2;
+    public Transform p2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void massChanger()
+    {
+        if (rb1.mass == 3 && Input.GetKeyDown("s"))
+        {
+            rb1.mass -= 1;
+            rb2.mass += 1;
+
+        }
+        else if (rb1.mass == 2 && Input.GetKeyDown("s"))
+        {
+            rb1.mass -= 1;
+            rb2.mass += 1;
+        }
+        else if (rb1.mass == 1 && Input.GetKeyDown("s"))
+        {
+            
+        }
+
+        if(rb2.mass == 3 && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rb1.mass += 1;
+            rb2.mass -= 1;
+        }
+        else if(rb2.mass == 2 && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rb1.mass += 1;
+            rb2.mass -= 1;
+        }
+        else if(rb2.mass == 1 && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }
+
+    }
+
+    public void scaleChanger()
+    {
+        if(rb1.mass == 1 && rb2.mass == 3)
+        {
+            p1.localScale = new Vector3(1, 1, 1);
+            p2.localScale = new Vector3(3, 3, 3);
+        }
+        else if(rb1.mass == 2 && rb2.mass == 2)
+        {
+            p1.localScale = new Vector3(2, 2, 2);
+            p2.localScale = new Vector3(2, 2, 2);
+        }
+        else if (rb1.mass == 3 && rb2.mass == 1)
+        {
+            p1.localScale = new Vector3(3, 3, 3);
+            p2.localScale = new Vector3(1, 1 ,1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        massChanger();
+        scaleChanger();
+    }
+}
---
+++ Co-op/Assets/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject player1, player2;
+    public Rigidbody rb1, rb2;
+    private float speed;
+    public float jumpForce = 100;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void p1Movement()
+    {
+        if (Input.GetKey("a"))  //Left
+        {
+            //player1.transform.position += new Vector3(0, 0, -speed * Time.deltaTime);
+            rb1.AddForce(0, 0, -speed);
+        }
+
+        if (Input.GetKey("d"))  //Right
+        {
+            rb1.AddForce(0, 0, speed);
+        }
+
+        if (Input.GetKeyDown("w"))  //Jump
+        {
+            rb1.AddForce(0, jumpForce, 0);
+        }
+
+    }
+
+    void p2Movement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))  //Left
+        {
+            rb2.AddForce(0, 0, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))  //Right
+        {
+            rb2.AddForce(0, 0, speed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))  //Jump
+        {
+            rb2.AddForce(0, jumpForce, 0);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = 50;
+        p1Movement();
+        p2Movement();
+    }
+}
